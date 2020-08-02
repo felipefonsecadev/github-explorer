97b74991f3780422e079e9ba6d4abbf21afb352f
Fipas <fonsecafel@gmail.com>
2020-08-02 20:05:26 -0300

    Add styling to form and repositories list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositorio" type="text" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2052767?s=460&u=55ece77c1db891da58f7f02c6a3c3970ecaef6ab&v=4"
+            alt="Felipe Fonseca"
+          />
+          <div>
+            <strong>felipefonsecadev/gobarber-backend</strong>
+            <p>Backend do app GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2052767?s=460&u=55ece77c1db891da58f7f02c6a3c3970ecaef6ab&v=4"
+            alt="Felipe Fonseca"
+          />
+          <div>
+            <strong>felipefonsecadev/gobarber-backend</strong>
+            <p>Backend do app GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/2052767?s=460&u=55ece77c1db891da58f7f02c6a3c3970ecaef6ab&v=4"
+            alt="Felipe Fonseca"
+          />
+          <div>
+            <strong>felipefonsecadev/gobarber-backend</strong>
+            <p>Backend do app GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
